c127b73b7f688baed8229287c567a37c688f7f21
Emmanuel_OKON_TEHC <e.okon@tehcoop.com>
2025-04-01 02:25:12 +0100

    Creation of Login/register endpoints

---
+++ TaskHub/Controller/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskHub.TaskHub.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskHub.TaskHub.Controller
+{
+    public class AuthenticationController {
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegisterDto model)
+		{
+			var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+			var result = await _userManager.CreateAsync(user, model.Password);
+
+			if (!result.Succeeded) return BadRequest(result.Errors);
+
+			return Ok(new { message = "User registered successfully" });
+		}
+
+		[HttpPost("login")]
+		public async Task<IActionResult> Login([FromBody] LoginDto model)
+		{
+			var user = await _userManager.FindByEmailAsync(model.Email);
+			if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+				return Unauthorized(new { message = "Invalid credentials" });
+
+			var token = _tokenService.GenerateToken(user);
+			return Ok(new { token });
+		}
+	}
+
+}
